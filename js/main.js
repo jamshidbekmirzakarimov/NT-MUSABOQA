a8ec12ade781d971d0a7d17ddfa1a6b9dc338bf2
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-03-11 17:59:11 +0500

    header responsive finished

--- js/main.js
+++ js/main.js
@@ -12,3 +12,26 @@ function changeTheme() {
   }
   target.classList.toggle("theme-dark");
 }
+
+
+let elBurgetBtn = document.querySelector(".site-header__burger-btn");
+let elBurgetMenu = document.querySelector(".site-header__menu-burger");
+let elCloseBtn = document.querySelector(".site-header__close-btn");
+let elWrapper = document.querySelector(".none");
+
+elBurgetBtn.addEventListener("click", function(){
+  elBurgetMenu.classList.add("site-header__add-class");
+  elWrapper.style.display = "none";
+})
+
+elCloseBtn.addEventListener("click", function(){
+  elBurgetMenu.classList.remove("site-header__add-class");
+  elWrapper.style.display = "block";
+})
+
+window.onclick = function(e) {
+  // console.log(e.target)
+  if(e.target == elBurgetMenu){
+    elBurgetMenu.classList.remove("site-header__add-class");
+  }
+};
